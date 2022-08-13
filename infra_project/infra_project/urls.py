738f80f70409fd25df7d7f5036be4433338c912d
Marina Sudoplatova <97099985+Marina-lq@users.noreply.github.com>
2022-08-13 12:36:45 +0300

    test5

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    path('', include('infra_app.urls', namespace='infra_app')),
-    path('admin/', admin.site.urls),
+    path("", include("infra_app.urls", namespace="infra_app")),
+    path("admin/", admin.site.urls),
 ]
